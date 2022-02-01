0d01ec80d81c93e0c3f6b0c53b2ffac35a4f5894
rhkearns <ryankearns217@gmail.com>
2022-02-01 16:37:42 -0500

    refactor scss folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() { 
-  return src('*.scss')
+  return src('fidensCSS/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['*.scss'], buildStyles)
+  watch(['fidensCSS/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
