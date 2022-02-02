25e9033bac94b8efcc6b2b23038d3fd42cff704b
rhkearns <ryankearns217@gmail.com>
2022-02-02 17:13:19 -0500

    add badge, tutorial done

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
-function buildStyles() { 
-  return src('fidensCSS/**/*.scss')
-    .pipe(sass())
-    .pipe(dest('css'))
+function buildStyles() {
+	return src('fidensCSS/**/*.scss')
+		.pipe(sass())
+		.pipe(purgecss({ content: ['*.html'] }))
+		.pipe(dest('css'));
 }
 
 function watchTask() {
-  watch(['fidensCSS/**/*.scss'], buildStyles)
+	watch(['fidensCSS/**/*.scss', '*.html'], buildStyles);
 }
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
